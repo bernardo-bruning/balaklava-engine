611fa49ce0cc0c50149bfb3ad5a00c7a2bcaad12
Bernardo Bruning <bernardo.bruning@gmail.com>
2020-03-04 00:10:08 -0300

    implemented method load from obj loader

--- src/loaders/mod.rs
+++ src/loaders/mod.rs
@@ -1,14 +1,28 @@
 use crate::geometry::Mesh;
+use tobj::{Model, Material};
 use std::path::Path;
-use std::io::Result;
 use std::fs::File;
+use std::error::Error;
 
-
-pub struct Obj {}
+pub struct Obj {
+    models: Vec<Model>,
+    materials: Vec<Material>
+}
 
 impl Obj {
-    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self> {
-        unimplemented!();
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, String> {
+        let result = tobj::load_obj(path.as_ref());
+        if result.is_err() {
+            let error = result.unwrap_err();
+            return Result::Err(String::from(error.description()));
+        }
+
+        let (models, materials) = result.unwrap();
+
+        return Result::Ok(Obj {
+            models,
+            materials
+        })
     }
 }
 
