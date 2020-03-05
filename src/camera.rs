fdbd8f152d3ffaab04730caf10a690cfe5cce9db
Bernardo Bruning <bernardo.bruning@gmail.com>
2020-03-04 23:25:52 -0300

    remove method default ortographic

--- src/camera.rs
+++ src/camera.rs
@@ -1,10 +1,6 @@
 extern crate nalgebra as na;
 use na::{Matrix4, Point3, Orthographic3, Vector3, Translation3};
 
-fn DefaultOrtographic3() -> Matrix4<f32> {
-    return Orthographic3::new(-10., 10., -10., 10., 0., 100.).to_homogeneous();
-}
-
 pub struct Orthographic {
     projection: Matrix4<f32>,
     view: Matrix4<f32>
@@ -12,7 +8,7 @@ pub struct Orthographic {
 
 impl Orthographic {
     pub fn new(position: Point3<f32>, direction: Point3<f32>) -> Self {
-        let projection = DefaultOrtographic3();
+        let projection = Orthographic3::new(-10., 10., -10., 10., 0., 100.).to_homogeneous();;
         let view = Matrix4::look_at_rh(&position, &direction, &Vector3::new(0., 1., 0.));
 
         return Self{
