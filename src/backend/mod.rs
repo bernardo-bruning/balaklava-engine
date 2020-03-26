352145d43de5b35fa5dadb31e6932b60d348e379
Bernardo Bruning <bernardo.bruning@gmail.com>
2020-03-25 23:46:24 -0300

    include ShaderProgram

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,6 +1,20 @@
 use crate::graphics::{Bindable, Texture};
 pub mod gfx;
 
+pub struct ShaderProgram {
+    pub vertex_shader: String,
+    pub fragment_shader: String
+}
+
+impl ShaderProgram {
+    fn new(vertex_shader: String, fragment_shader: String) -> Self {
+        ShaderProgram {
+            vertex_shader,
+            fragment_shader
+        }
+    }
+}
+
 pub trait Graphics<TR> : Bindable<Texture<TR>> {}
 
 pub trait Backend <TR, G: Graphics<TR>> {
