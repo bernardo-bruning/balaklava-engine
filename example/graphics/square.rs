447bb2dd14f09e9205a0e4ed94c0c0857624de36
Bernardo Bruning <bernardo.bruning@gmail.com>
2020-04-06 23:47:54 -0300

    delete files not used

--- example/graphics/square.rs
+++ example/graphics/square.rs
@@ -1,35 +1,34 @@
 extern crate balaklava;
+use balaklava::{Application, lauch_gfx};
 use balaklava_gfx::config::Config;
-use balaklava_gfx::GfxDevice;
+use balaklava_gfx::{GfxDevice};
 use balaklava_gpu::{Device, Vector};
-use balaklava_gfx::glutin::{Event, WindowEvent, EventsLoop};
-use std::*;
 
+struct Game <D: Device> {
+    program: D::Program
+}
+
+impl<D: Device> Application<D> for Game<D> {
+    fn new(device: &mut D) -> Self {
+        let vertex_shader = include_bytes!("shader/shader_150.glslv");
+        let pixel_shader = include_bytes!("shader/shader_150.glslf");
+        let mut vertices = Vec::new();
+        vertices.push(Vector::new(0.0, 1.0, 0.0));
+        vertices.push(Vector::new(-1.0, -1.0, 0.0));
+        vertices.push(Vector::new(1.0, -1.0, 0.0));
+
+        let program = device.create_program(vertex_shader.to_vec(), pixel_shader.to_vec(), vertices);
+        
+        Game {
+            program
+        }
+    }
+    
+    fn render(&mut self, device: &mut D) {
+        device.render_program(&self.program);
+    }
+}
 
 fn main() {
-    let config = Config::default();
-    let mut events_loop = EventsLoop::new();
-    let mut device = GfxDevice::new(config, &events_loop);
-    let vertex_shader = include_bytes!("shader/shader_150.glslv");
-    let pixel_shader = include_bytes!("shader/shader_150.glslf");
-    let mut vertices = Vec::new();
-    vertices.push(Vector::new(0.0, 1.0, 0.0));
-    vertices.push(Vector::new(-1.0, -1.0, 0.0));
-    vertices.push(Vector::new(1.0, -1.0, 0.0));
-
-    let program = device.create_program(vertex_shader.to_vec(), pixel_shader.to_vec(), vertices);
-    let mut running = true;
-
-    while running {
-        events_loop.poll_events(|event| match event {
-            Event::WindowEvent{ event, .. } => match event {
-                WindowEvent::Closed => running = false,
-                _ => ()
-            },
-            _ => ()
-        });
-
-        device.render_program(&program);
-        device.flush();
-    }
+    lauch_gfx::<Game<GfxDevice>>(Config::default());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,30 @@
-extern crate nalgebra as na;
-extern crate log;
-extern crate env_logger;
+use balaklava_gpu::Device;
+use balaklava_gfx::config::Config;
+use balaklava_gfx::GfxDevice;
+use balaklava_gfx::glutin::{EventsLoop, Event, WindowEvent};
 
-mod core;
-mod geometry;
-mod camera;
-mod loaders;
+pub trait Application<D: Device> {
+    fn new(device: &mut D) -> Self;
+    fn render(&mut self, device: &mut D);
+}
 
-// impl Application for Example {
 
-//     fn create(&mut self) {
-//         env_logger::init();
-//         info!("Initialize program");
-//         let builder = core::Builder::default()
-//             .with_name("Learn GFX".to_string())
-//             .with_light(Light {
-//                 position: [0.5, 0.0, 0.07, 1.0],
-//                 color: [1.0, 1.0, 1.0]
-//             });
+pub fn lauch_gfx<A: Application<GfxDevice>>(config: Config) {
+    let mut events_loop = EventsLoop::new();
+    let mut device = GfxDevice::new(config, &events_loop);
+    let mut app = A::new(&mut device);
 
-//         let obj_result = Obj::open("./penguin.obj");
-//         if obj_result.is_err() {
-//             let error: String = obj_result.unwrap_err();
-//             panic!("One error ocurred: {}", error);
-//         }
+    let mut running = true;
+    while running {
+        events_loop.poll_events(|event| match event {
+            Event::WindowEvent{ event, .. } => match event {
+                WindowEvent::Closed => running = false,
+                _ => ()
+            },
+            _ => ()
+        });
 
-//         let obj = obj_result.unwrap();
-//         let r: Result<u32, String> = Result::Err("test".to_string());
-//         let model_result: Result<geometry::Mesh, String> = obj.into();
-//         if model_result.is_err() {
-//             panic!("One error ocurred when convert Obj into Mesh: {}", model_result.unwrap_err());
-//         }
-
-//         let mut model = model_result.unwrap();
-//         model.texture = Texture::load("texture.png".to_string()).ok();
-        
-//         let mut engine = std::boxed::Box::from(builder.build());
-
-//         let mut running = true;
-//         info!("Initialize render");
-//         while running {
-//             model.set_rotation(na::Vector3::new(0., 0.01, 0.));
-//             //engine.camera.translate(0., 0., -0.01);
-//             engine.poll_event(|event| match event {
-//                 events::Event::Closed => running = false,
-//                 events::Event::KeyInput{ input } => 
-//                     match input.virtual_key {
-//                         //Option::Some(events::KeyCodes::W) => engine.camera.translate(1., 0., 0.),
-//                         //Option::Some(events::KeyCodes::W) => println!("teste"),
-//                         _ => ()
-//                     },
-//                 _ => ()
-//             });
-
-//             engine.clear();
-//             model.render(&mut engine);
-//             engine.update();
-//         }
-//         info!("Close program");
-//     }
-
-//     fn render(&mut self) {
-
-//     }
-// }
+        app.render(&mut device);
+        device.flush();
+    }
+}
