bf8ebd3cd738c7ccc12a6f7fc0b4196f0099296e
Bernardo Bruning <bernardo.bruning@gmail.com>
2020-04-08 00:23:14 -0300

    implement create_program

--- src/gl/src/lib.rs
+++ src/gl/src/lib.rs
@@ -1,24 +1,28 @@
 extern crate glium;
 
+use glium::{Display};
+use glium::glutin::ContextBuilder;
+use glium::glutin::window::WindowBuilder;
 use glium::glutin::event_loop::EventLoop;
 use balaklava_gpu::{Device, Vector};
 
 pub struct Program {
-
+    inner_program: glium::Program,
+    vertices: Vec<Vector>
 }
 
 pub struct GlDevice {
-    display: glium::Display
+    display: Display
 }
 
 impl GlDevice {
     pub fn new<T: 'static>(events_loop: &EventLoop<T>) -> Self {
-        let window_builder = glium::glutin::window::WindowBuilder::new()
+        let window_builder = WindowBuilder::new()
             .with_title("balaklava engine");
         
-        let context_builder = glium::glutin::ContextBuilder::new();
+        let context_builder = ContextBuilder::new();
 
-        let display_result = glium::Display::new(window_builder, context_builder, &events_loop);
+        let display_result = Display::new(window_builder, context_builder, &events_loop);
         let display = display_result.unwrap();
 
         GlDevice{
@@ -31,7 +35,13 @@ impl Device for GlDevice {
     type Program = Program;
 
     fn create_program(&mut self, vertex_shader: Vec<u8>, pixel_shader: Vec<u8>, vertices: Vec<Vector>) -> Self::Program {
-        unimplemented!();
+        let vertex = std::str::from_utf8(vertex_shader.as_ref()).unwrap();
+        let pixel = std::str::from_utf8(pixel_shader.as_ref()).unwrap();
+        let program_result = glium::Program::from_source(&self.display, vertex, pixel, None);
+        return Program {
+            inner_program: program_result.unwrap(),
+            vertices: vertices
+        }
     }
 
     fn render_program(&mut self, program: &Self::Program) {
