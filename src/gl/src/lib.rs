03a6339bb0c78d4dff6fb03aea1bcf2202a0a3e1
Bernardo Bruning <bernardo.bruning@gmail.com>
2020-04-08 00:14:08 -0300

    include new from gl device

--- src/gl/src/lib.rs
+++ src/gl/src/lib.rs
@@ -1,17 +1,28 @@
 extern crate glium;
 
+use glium::glutin::event_loop::EventLoop;
 use balaklava_gpu::{Device, Vector};
 
 pub struct Program {
 
 }
 
-pub struct GlDevice {}
+pub struct GlDevice {
+    display: glium::Display
+}
 
 impl GlDevice {
-    fn new() -> Self {
-        GlDevice{
+    pub fn new<T: 'static>(events_loop: &EventLoop<T>) -> Self {
+        let window_builder = glium::glutin::window::WindowBuilder::new()
+            .with_title("balaklava engine");
+        
+        let context_builder = glium::glutin::ContextBuilder::new();
 
+        let display_result = glium::Display::new(window_builder, context_builder, &events_loop);
+        let display = display_result.unwrap();
+
+        GlDevice{
+            display
         }
     }
 }
