ef04f7ebf5fd125014ca6fb5bc8f454ad2c374e4
Bernardo Bruning <bernardo.bruning@gmail.com>
2020-04-13 23:34:26 -0300

    update example

--- example/graphics/square.rs
+++ example/graphics/square.rs
@@ -4,7 +4,8 @@ use balaklava_gl::GlDevice;
 use balaklava_gpu::{Device, Vector};
 
 struct Game <D: Device> {
-    program: D::Program
+    program: D::Program,
+    vertices: D::Buffer
 }
 
 impl<D: Device> Application<D> for Game<D> {
@@ -15,16 +16,17 @@ impl<D: Device> Application<D> for Game<D> {
         vertices.push(Vector::new(0.0, 1.0, 0.0));
         vertices.push(Vector::new(-1.0, -1.0, 0.0));
         vertices.push(Vector::new(1.0, -1.0, 0.0));
-
-        let program = device.create_program(vertex_shader.to_vec(), pixel_shader.to_vec(), vertices);
+        let mut program = device.create_program(vertex_shader.to_vec(), pixel_shader.to_vec());
+        let buffer = device.create_vertex_buffer(&mut program, vertices);
         
         Game {
-            program
+            program,
+            vertices: buffer
         }
     }
     
     fn render(&mut self, device: &mut D) {
-        device.render_program(&self.program);
+        device.render_program(&self.program, &self.vertices, Option::None, Option::None);
     }
 }
 
