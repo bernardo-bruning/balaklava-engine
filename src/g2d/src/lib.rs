35be136d2ac6613147a0dce874a1b7936353e4a9
Bernardo Bruning <bernardo.bruning@gmail.com>
2020-04-16 00:44:08 -0300

    create sprite

--- src/g2d/src/lib.rs
+++ src/g2d/src/lib.rs
@@ -1,9 +1,16 @@
+use std::path::PathBuf;
 use balaklava_gpu::Device;
 
-struct Sprite {}
+pub struct Sprite<D: Device> {
+    path: PathBuf,
+    texture: Option<D::Texture>
+}
 
-impl Sprite {
-    fn new() -> Self {
-        Sprite{}
+impl <D: Device> From<PathBuf> for Sprite<D> {
+    fn from(path: PathBuf) -> Self{
+        Sprite::<D>{
+            path: path,
+            texture: Option::None
+        }
     }
 }
